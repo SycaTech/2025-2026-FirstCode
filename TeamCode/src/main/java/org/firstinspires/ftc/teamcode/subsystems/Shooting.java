3555f76a98eec25edf50e21c0bcbcc3af9368d80
ProProgrammer1234 <127347148+ProProgrammer1234@users.noreply.github.com>
2025-11-14 15:11:50 +0200

    All mechanisms
    עבדנו על כל המנגנונים תעשו פול

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Shooting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Shooting.java
@@ -1,9 +1,29 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
+
+import dev.nextftc.core.commands.Command;
+import dev.nextftc.core.commands.utility.InstantCommand;
+import dev.nextftc.hardware.impl.ServoEx;
+
 public class Shooting {
-    private DcMotorEx shootmotor;
-    private Servo shootservo
+    private DcMotorEx Shootmotor;
+    private ServoEx ShootServo;
+
+    public void init(HardwareMap hwMap) {
+        Shootmotor = hwMap.get(DcMotorEx.class, "ShootMotor");
+        ShootServo = hwMap.get(ServoEx.class, "ShootServo");
+    }
+
+   public Command servopower(){
+        return new InstantCommand(() -> {
+            ShootServo.setPosition(1.0);
+        });
+   };
+   public void power(){
+       Shootmotor.setPower(1.0);
+}
 }
