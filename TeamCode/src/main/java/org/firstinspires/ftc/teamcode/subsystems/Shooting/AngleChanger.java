bf1048a57da0f6c6dc066e4c48bb5afe71e0d259
ProProgrammer1234 <127347148+ProProgrammer1234@users.noreply.github.com>
2025-11-18 22:45:10 +0200

    Update AngleChanger.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Shooting/AngleChanger.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Shooting/AngleChanger.java
@@ -3,10 +3,18 @@ package org.firstinspires.ftc.teamcode.subsystems.Shooting;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
+import dev.nextftc.core.commands.Command;
+
 public class AngleChanger {
     private Servo AngleChangeServo;
 
     public void init(HardwareMap hwMap) {
         AngleChangeServo = hwMap.get(Servo.class, "AngleChangeServo");
     }
+
+    public void setServoPos(double angle) {
+        AngleChangeServo.setPosition(angle);
+    }
+
+    public Command changeAngle
 }
