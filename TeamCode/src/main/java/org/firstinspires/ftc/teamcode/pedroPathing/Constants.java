68aac1ed18422b796eb851bc6254fbda6869715d
SycaTech <148126462+SycaTech@users.noreply.github.com>
2025-11-21 09:51:01 +0200

    pedro poathing
    change

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -4,50 +4,49 @@ import com.pedropathing.follower.Follower;
 import com.pedropathing.follower.FollowerConstants;
 import com.pedropathing.ftc.FollowerBuilder;
 import com.pedropathing.ftc.drivetrains.MecanumConstants;
-import com.pedropathing.ftc.localization.constants.PinpointConstants;
+import com.pedropathing.ftc.localization.Encoder;
+import com.pedropathing.ftc.localization.constants.DriveEncoderConstants;
 import com.pedropathing.paths.PathConstraints;
-import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
-import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
-import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
-
 public class Constants {
-    public static FollowerConstants followerConstants = new FollowerConstants();
+    public static FollowerConstants followerConstants = new FollowerConstants()
+            .mass(5);
+    public static MecanumConstants driveConstants = new MecanumConstants()
+            .maxPower(1)
+            .rightFrontMotorName("RF")
+            .rightRearMotorName("RR")
+            .leftRearMotorName("LR")
+            .leftFrontMotorName("LF")
+            .leftFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
+            .leftRearMotorDirection(DcMotorSimple.Direction.REVERSE)
+            .rightFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
+            .rightRearMotorDirection(DcMotorSimple.Direction.FORWARD);
 
-    {
-        followerConstants.mass(5.1);
-    }
-
-        public static MecanumConstants driveConstants = new MecanumConstants()
-                .maxPower(1)
-                .rightFrontMotorName("RF")
-                .rightRearMotorName("RR")
-                .leftRearMotorName("LR")
-                .leftFrontMotorName("LF")
-                .leftFrontMotorDirection(DcMotor.Direction.REVERSE)
-                .leftRearMotorDirection(DcMotor.Direction.REVERSE)
-                .rightFrontMotorDirection(DcMotor.Direction.FORWARD)
-                .rightRearMotorDirection(DcMotor.Direction.FORWARD);
-
-    public static PinpointConstants localizerConstants = new PinpointConstants()
-            .forwardPodY(-182/2.54)
-            .strafePodX(-196/2.54)
-            .distanceUnit(DistanceUnit.INCH)
-            .hardwareMapName("pinpoint")
-            .customEncoderResolution(4000/(2 * Math.PI * 16))
-            .forwardEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD)
-            .strafeEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD);
-
+    public static DriveEncoderConstants localizerConstants = new DriveEncoderConstants()
+            .rightFrontMotorName("RF")
+            .rightRearMotorName("RR")
+            .leftRearMotorName("LR")
+            .leftFrontMotorName("LF")
+            .leftFrontEncoderDirection(Encoder.FORWARD)
+            .leftRearEncoderDirection(Encoder.FORWARD)
+            .rightFrontEncoderDirection(Encoder.FORWARD)
+            .rightRearEncoderDirection(Encoder.FORWARD)
+            .robotWidth(14.961)
+            .robotLength(14.37)
+            .forwardTicksToInches(8.186938634728);
 
     public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
 
     public static Follower createFollower(HardwareMap hardwareMap) {
         return new FollowerBuilder(followerConstants, hardwareMap)
-                .pathConstraints(pathConstraints)
                 .mecanumDrivetrain(driveConstants)
+                .driveEncoderLocalizer(localizerConstants)
+                .pathConstraints(pathConstraints)
                 .build();
+        }
+
+
     }
 
-}
