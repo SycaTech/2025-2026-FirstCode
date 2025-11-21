95f0efa3c93a06acbb5db7ef3445c35c385bd003
ProProgrammer1234 <127347148+ProProgrammer1234@users.noreply.github.com>
2025-11-21 15:54:04 +0200

    Lift shoulder

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/LiftShoulder.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/LiftShoulder.java
@@ -1,28 +1,35 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.gamepad1;
+
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 import dev.nextftc.control.ControlSystem;
 import dev.nextftc.core.commands.Command;
+import dev.nextftc.core.commands.groups.ParallelGroup;
 import dev.nextftc.core.commands.utility.InstantCommand;
 import dev.nextftc.hardware.impl.MotorEx;
 
 public class LiftShoulder{
+    Telemetry telemetry;
 
     private MotorEx LiftShoulderL;
     private MotorEx LiftShoulderR;
 
     public static final int POSE_LOW = 0;
     public static final int POSE_MIDDLE = 500;
-    public static final int POSE_HIGH = 700;
+    public static final int POSE_HIGH = 1200;
 
     public void init(HardwareMap hwMap) {
-        LiftShoulderL = hwMap.get(MotorEx.class, "Master");
-        LiftShoulderR = hwMap.get(MotorEx.class, "Slave");
+        LiftShoulderL = new MotorEx(hwMap.get(DcMotorEx.class, "Master"));
+        LiftShoulderR = new MotorEx(hwMap.get(DcMotorEx.class, "Slave"));
     }
     public ControlSystem controlSystem = ControlSystem.builder()
-            .posPid(0.005, 0.0001, 0.0)
-            .elevatorFF(0.1)
+            .posPid(0.0, 0.0, 0.0)
+            .elevatorFF(0.0)
             .build();
 
     public Command toHigh(){
@@ -37,6 +44,13 @@ public class LiftShoulder{
 
     public Command toSetPoint(int setpoint) {
         return new InstantCommand(()-> LiftShoulderL.atPosition(setpoint));
+    }
+
+    public Command power() {
+        return new ParallelGroup(
+                new InstantCommand(() -> LiftShoulderL.setPower(0.5)),
+                new InstantCommand(() -> LiftShoulderR.setPower(0.5))
+        );
     }
 
     public void periodic() {
